38a6e9e8cef15fdda593f003d96e9e71393982e0
cffampo <cffampo@gmail.com>
2025-05-26 21:16:33 -0400

    Finished logic and the rest of the game

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3) //random number. either 0, 1, or 2
 
@@ -13,13 +17,73 @@ function getComputerChoice() {
     return choice;
 }
 
-console.log(getComputerChoice());
-
 function getHumanChoice() {
     let choice = prompt("Rock, Paper, or Scissors: ");
     
     return choice;
 }
 
-console.log(getHumanChoice());
+function playRound(playerChoice, computerChoice) {
+    playerChoice = playerChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+
+    if (playerChoice == "rock") {
+        if (computerChoice == "rock") {
+            console.log("It's a tie!");
+        } else if (computerChoice == "paper") {
+            console.log("You lose! Rock loses to Paper");
+            computerScore++;
+        } else {
+            console.log("You win! Rock beats Scissors!");
+            humanScore++;
+        }
+    }
+
+    if (playerChoice == "paper") {
+        if (computerChoice == "paper") {
+            console.log("It's a tie!");
+        } else if (computerChoice == "scissors") {
+            console.log("You lose! Paper loses to Scissors!");
+            computerScore++;
+        } else {
+            console.log("You win! Paper beats Rock!");
+            humanScore++;
+        }
+    }
+
+    if (playerChoice == "scissors") {
+        if (computerChoice == "scissors") {
+            console.log("It's a tie!");
+        } else if (computerChoice == "rock") {
+            console.log("You lose! Scissors loses to Rock!");
+            computerScore++;
+        } else {
+            console.log("You win! Scissors beats Paper!");
+            humanScore++;
+        }
+    }
+}
+
+
+
+function playGame() {
+
+    for(let i = 0; i < 5; i++){
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+
+        playRound(humanSelection, computerSelection);
+    }
+
+    if(humanScore > computerScore){
+        console.log("You win!");
+    } else if(humanScore < computerScore){
+        console.log("You lose!");
+    } else {
+        console.log("It's a tie!")
+    }
+}
+playGame();
+
+
 
